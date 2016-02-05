d3346d2e8fdd03f049640ff07283ac700148364c
d1son <dson120@gmail.com>
2016-02-04 20:57:51 -0500

    started the js while loop exercises, need to finish the last 2

---
+++ lecture_89_whileLoops_exercise/while_loops.js
@@ -0,0 +1,14 @@
+// print all numbers beteween -10 and 19
+var count = -10
+
+while(count <= 19) {
+	console.log(count);
+	count++;
+}
+
+var number = 10
+
+while(number <= 40){
+	console.log(number);
+	number++;
+};
