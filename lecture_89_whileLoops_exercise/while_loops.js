51d7c9b88f5daee76a2f92a7e72fe6edb47080ab
d1son <dson120@gmail.com>
2016-02-09 18:10:44 -0500

    finished while loop exercises

--- lecture_89_whileLoops_exercise/while_loops.js
+++ lecture_89_whileLoops_exercise/while_loops.js
@@ -1,14 +1,35 @@
 // print all numbers beteween -10 and 19
-var count = -10
+var counter = -10
 
-while(count <= 19) {
-	console.log(count);
-	count++;
+while(counter <= 19) {
+	console.log(counter);
+	counter++;
+};
+
+// print all even numbers between 10 and 40
+var counter = 10
+
+while(counter <= 40) {
+	console.log(counter);
+	counter+=2;
+};
+
+//print all odd numbers between 300 and 333
+var counter = 300
+
+while(counter <= 333) {
+	if (counter % 2 !== 0) {
+		console.log(counter);
+	}
+	counter+=1;
 }
 
-var number = 10
+//print all numbers divisible by 5 AND 3 between 5 and 50
+ var counter = 5
 
-while(number <= 40){
-	console.log(number);
-	number++;
-};
+ while(counter <= 50) {
+ 	if (counter % 5 === 0 && counter % 3 === 0) {
+ 		console.log(counter);
+ 	}
+ 	counter+=1;
+ };
