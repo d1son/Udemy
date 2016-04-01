102febc896269ad321130e7c7e51b698444cccba
d1son <dson120@gmail.com>
2016-03-31 23:19:40 -0400

    finished array iteration exercises, need more practice

---
+++ arrays_exercise/array.js
@@ -0,0 +1,36 @@
+// write a function printReverse() that takes an array as an argument and prints out the elements in the array in reverse order
+
+function printReverse(arr){
+	for(var i = arr.length - 1; i >= 0; i--) {
+		console.log(arr[i]);
+	}
+};
+
+printReverse([1,2,3,4,5]);
+
+// write a function isUniform() which takes an array as an argument and returns true if all elements in the array are identical
+
+
+function isUniform(arr) {
+	var first = arr[0];
+	for(var i = 1; i < arr.length; i++) {
+		if(arr[i] !== first) {
+			return false;
+		}
+	}
+	return true;
+};
+
+isUniform([1,1,1,'a']); //need to call the function in the js console, or put this inside a console.log
+
+// write a function sumArray() that accepts an array of numbers and returns the sum of all numbers in the array
+
+function sumArray(arr) {
+	var total = 0;
+	arr.forEach(function(element) {
+		total += element;
+	});
+	return total;
+};
+
+console.log(sumArray([1,3,2,2])); 
